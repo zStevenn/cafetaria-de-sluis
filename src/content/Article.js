ec706aa8a4dfbdca16e48eceb2e1a4c6581dd45c
zStevenn <56149788+zStevenn@users.noreply.github.com>
2022-11-07 12:02:00 +0100

    Create Article.js

---
+++ src/content/Article.js
@@ -0,0 +1,29 @@
+import "./Article.css";
+
+function ContentProduct(props) {
+  return (
+    <div className="article__card">
+      <h3 className="article__title">Titel {props.id}</h3>
+      <img className="article__img" src={props.id}/>
+      <p className="article__description">
+      <span className="article__price">€ 5,99</span>
+        Korte omschrijving
+      </p>
+    </div>
+  );
+}
+
+function DisplayArticles(props) {
+  const generateBlocks = props.block.map((array) =>
+    <ContentProduct key={array} id={array}/>
+  );
+  return (
+    <div className="article">
+      {generateBlocks}
+    </div>
+  );
+}
+
+const blockArray = [1,2,3,4,5,6,7,8,9];
+
+export default Article;
