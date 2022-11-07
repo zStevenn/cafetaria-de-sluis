d55455757a7db06630b7977e7df78a7eaeb1de66
zStevenn <56149788+zStevenn@users.noreply.github.com>
2022-11-07 09:43:20 +0100

    Added hamburger open icon to Header.js

--- src/header/Header.js
+++ src/header/Header.js
@@ -26,6 +26,12 @@ function HeaderMenu() {
 			<path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>
 		</svg>
 	);
+	
+	const MenuOpenIcon = (
+		<svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">
+			<path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"/>
+		</svg>
+	);
 
 	function toggleMenu() {
 		setMenuOpen(!menuOpen);
