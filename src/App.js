62be74a67c05b6236e203144dbf16f462176b1be
zStevenn <56149788+zStevenn@users.noreply.github.com>
2022-11-08 10:28:22 +0100

    Modified component imports

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import "./App.css";
 
 // Import components
 import Header from "./components/layout/Header";
-import ContentOrder from "./components/content/ContentOrder";
+import Banner from "./components/content/Banner";
 import ContentReview from "./components/content/ContentReview";
 import Footer from "./components/content/Footer";
-import Article from "./components/content/Article";
+import Products from "./components/content/Products";
 
 function App() {
 	return (
