33b5d427b6ebdad0c57cb8d47b83446ff307299e
zStevenn <steventje678@hotmail.com>
2022-11-22 13:47:23 +0100

    Added responsiveness to hero section

--- src/components/content/HeroSection.js
+++ src/components/content/HeroSection.js
@@ -1,19 +1,19 @@
 function HeroSection() {
 	return (
-		<div className="grid grid-rows-2 justify-center content-center my-2">
-			<div className="flex flex-col justify-center text-center">
+		<div className="flex flex-col justify-center p-2 md:flex-row">
+			<div className="flex flex-1 flex-col justify-center content-center text-center">
 				<div className="text-xl font-normal text-grey">Snel én makkelijk!</div>
-				<div className="text-3xl tracking-wide font-bold text-black">Bestel nu via onze eigen webshop</div>
-				<div className="mt-4">
+				<div className="text-3xl tracking-wide font-bold text-black mb-4">Bestel nu via onze eigen webshop</div>
+				<div>
 					<a
 						href="https://www.google.com"
-						className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+						className="bg-transparent hover:bg-green text-green font-semibold hover:text-white py-3 px-6 border border-green hover:border-transparent rounded-lg"
 					>
-						Bestellen
+						Bestel nu online!
 					</a>
 				</div>
 			</div>
-			<div className="flex justify-center content-center">
+			<div className="flex flex-1 justify-center content-center mt-4">
 				<img src="/images/hero__image.svg" alt="Hero image" />
 			</div>
 		</div>
