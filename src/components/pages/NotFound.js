c6ca347968ea69156d1ec4bc9da0b29cfda85a28
zStevenn <steventje678@hotmail.com>
2022-11-29 13:15:31 +0100

    Created navigate redirect on notfound page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function NotFound() {
-  return <>
-  <h1>404</h1>
-  </>
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		setTimeout(() => {
+			navigate("/");
+		}, 3000);
+	}, [navigate]);
+
+	return <h1>De pagina kon niet worden gevonden. U word doorverwezen naar de homepagina.</h1>
 }
