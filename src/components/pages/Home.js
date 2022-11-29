d99dcb2c1d5bce04f430929603a4d46eb25be929
zStevenn <steventje678@hotmail.com>
2022-11-29 13:19:33 +0100

    Adds components to homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,17 @@
+import HeroSection from '../content/HeroSection'
+import CTASection from '../content/CTASection'
+import ProductHighlights from '../content/ProductHighlights'
+import Review from '../content/Review'
+import Footer from '../content/Footer'
+
 export function Home() {
-  return <>
-  <h1>Home</h1>
-  </>
+	return (
+    <>
+      <HeroSection />
+      <CTASection />
+      <ProductHighlights />
+      <Review />
+      <Footer />
+    </>
+  );
 }
