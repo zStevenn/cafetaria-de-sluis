d74abca68d7a826da53e05251d96f1f5c1dd77dd
zStevenn <steventje678@hotmail.com>
2022-11-30 16:23:24 +0100

    Adds windowdimension hook

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,11 @@
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+
 export default function Footer() {
+  const { width } = useWindowDimensions();
+
   return (
-    <div className="flex flex-col justify-center content-center text-center">
-      <span className="text-3xl mx-2">&copy; Copyright Cafetaria de Sluis</span>
-    </div>
+    <>
+    {width > 640 ? "desktop" : "mobile"}
+    </>
   );
 }
