bd0b14de3747bf9c4f293554fb412ccca45f5b7c
zStevenn <steventje678@hotmail.com>
2022-11-30 16:51:16 +0100

    Inverts colors

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,17 +6,15 @@ export default function Navbar() {
 
 	return (
 		// Navbar
-		<div className="bg-white flex flex-col justify-center mx-4">
+		<div className="bg-green flex flex-col justify-center">
 			{/* Title */}
-			<div className="text-3xl font-bold font-sans text-green text-center my-2">Cafetaria de Sluis</div>
-			{/* Divider */}
-			<div className="h-1.5 bg-green my-2 rounded-full">&nbsp;</div>
+			<div className="text-3xl font-bold font-sans text-white text-center my-2">Cafetaria de Sluis</div>
 			{/* Menu Wrapper */}
 			<div className="flex flex-row py-2 justify-evenly">
 				{/* Menu */}
-        {width > 640 ? <DesktopNavigationMenu /> : <MobileNavigationMenu />}
+				{width > 640 ? <DesktopNavigationMenu /> : <MobileNavigationMenu />}
 				{/* Order Button */}
-				<a href="https://www.e-food.nl/driebergen/cafeteria-de-sluis" className="btn-primary">
+				<a href="https://www.e-food.nl/driebergen/cafeteria-de-sluis" className="btn-secondary">
 					Bestellen
 				</a>
 			</div>
