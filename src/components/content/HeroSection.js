3c313c3867c3ab5d70b24aed00ca128474e3551d
zStevenn <steventje678@hotmail.com>
2022-11-30 17:35:41 +0100

    Divider only shows on desktop

--- src/components/content/HeroSection.js
+++ src/components/content/HeroSection.js
@@ -3,12 +3,12 @@ import "./HeroSection.css";
 function HeroSection() {
 	return (
 		<div className="flex flex-col justify-center p-2 md:flex-row h-[40vh] relative">
-			<div class="waves-top">
+			<div class="waves-top hidden md:flex">
 				<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
 					<path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
 				</svg>
 			</div>
-			<div class="waves-bottom">
+			<div class="waves-bottom hidden md:flex">
 				<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
 					<path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
 				</svg>
