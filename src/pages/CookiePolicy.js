636384459716cb342a00a523f623b88de8984220
Steven <56149788+zStevenn@users.noreply.github.com>
2022-12-02 15:15:29 +0100

    Prettier CookiePolicy

--- src/pages/CookiePolicy.js
+++ src/pages/CookiePolicy.js
@@ -1,42 +1,93 @@
 export default function CookiePolicy() {
   return (
-    <div className="flex flex-col flex-nowrap w-10/12 sm:w-8/12 m-auto">
-				<h2 className="text-3xl font-semibold my-4 text-green">
-        Cookieverklaring
-				</h2>
-				<p className="font-semibold my-2 text-green">
-        1. Het gebruik van cookies
+    <div className='flex flex-col flex-nowrap w-10/12 sm:w-8/12 m-auto'>
+				<h2 className='text-3xl font-semibold my-4 text-green'>Cookieverklaring</h2>
+				<p className='font-semibold my-2 text-green'>1. Het gebruik van cookies</p>
+				<p>
+					www.cafetariadesluis.nl maakt gebruik van cookies. Een cookie is een eenvoudig klein bestandje dat met
+					pagina’s van deze website en/of Flash-applicaties wordt meegestuurd en door je browser op je harde schijf van
+					je computer, mobiele telefoon, smart watch of tablet wordt opgeslagen. De daarin opgeslagen informatie kan bij
+					een volgend bezoek weer naar onze servers teruggestuurd worden.
 				</p>
 				<p>
-        www.cafetariadesluis.nl maakt gebruik van cookies. Een cookie is een eenvoudig klein bestandje dat met pagina’s van deze website en/of Flash-applicaties wordt meegestuurd en door je browser op je harde schijf van je computer, mobiele telefoon, smart watch of tablet wordt opgeslagen. De daarin opgeslagen informatie kan bij een volgend bezoek weer naar onze servers teruggestuurd worden.
-        </p>
-        <p>
-        Het gebruik van cookies is van groot belang voor het goed laten draaien van onze website, maar ook cookies waarvan je niet direct het effect ziet zijn zeer belangrijk. Dankzij de (anonieme) input van bezoekers kunnen we het gebruik van de website verbeteren en deze gebruiksvriendelijker maken.
-        </p>
-        <p className="font-semibold my-2 text-green">2. Toestemming voor het gebruik van cookies</p>
-        <p>Voor het gebruik van bepaalde cookies is jouw toestemming vereist. Voor de cookies die wij gebruiken is geen toestemming vereist. </p>
-        <p className="font-semibold my-2 text-green">3. Type cookies en hun doelstellingen</p>
-        <p>Wij gebruiken de volgende type cookies:</p>
-        <ul>
-          <li>Functionele cookies: hiermee kunnen we de website beter laten functioneren en is die gebruiksvriendelijker voor de bezoeker. Bijvoorbeeld: we slaan je inloggegevens op of wat je in je winkelmandje hebt gestopt. </li>
-        </ul>
-        <p className="font-semibold my-2 text-green">4. Je rechten met betrekking tot je gegevens</p>
-        <p>Je hebt het recht op inzage, rectificatie, beperking en verwijdering van persoonsgegevens. Daarnaast heb je recht van bezwaar tegen verwerking van persoonsgegevens en recht op gegevensoverdraagbaarheid. Je kunt deze rechten uitoefenen door ons een mail te sturen via info@cafetariadesluis.nl.</p>
-        <p>Om misbruik te voorkomen kunnen wij je daarbij vragen om je adequaat te identificeren. Wanneer het gaat om inzage in persoonsgegevens gekoppeld aan een cookie, vragen we je een kopie van het cookie in kwestie mee te sturen. Je kunt deze terugvinden in de instellingen van je browser.</p>
-        <p className="font-semibold my-2 text-green">5. Cookies blokkeren en verwijderen</p>
-        <p>Je kunt cookies te allen tijde eenvoudig zelf blokkeren en verwijderen via je internetbrowser. Ook kun je je internetbrowser zodanig instellen dat je een bericht ontvangt als er een cookie wordt geplaatst. Je kunt ook aangeven dat bepaalde cookies niet geplaatst mogen worden. Bekijk voor deze mogelijkheid de helpfunctie van je browser. Als je de cookies in je browser verwijdert, kan dat gevolgen hebben voor het prettige gebruik van deze website.</p>
-        <p>Sommige tracking cookies worden geplaatst door derden die onder meer via onze website advertenties aan je vertonen. Deze cookies kan je centraal verwijderen via youronlinechoices.com</p>
-        <p>Wees je er wel van bewust dat als je geen cookies wilt, wij niet meer kunnen garanderen dat onze Website helemaal goed werkt. Het kan zijn dat enkele functies van de site verloren gaan of zelfs dat je de website helemaal niet meer kunt bezoeken. Daarnaast betekent het weigeren van cookies ook niet dat je helemaal geen advertenties meer te zien krijgt. De advertenties zijn dan alleen niet meer toegesneden op jouw interesses en kun daardoor vaker worden herhaald.</p>
-        <p>Hoe je je instellingen kunt aanpassen, verschilt per browser. Raadpleeg indien nodig de helpfunctie van jouw browser, of klik op één van de onderstaande links om direct naar de handleiding van je browser te gaan.</p>
-        <p className="font-semibold my-2 text-green">6. Nieuwe ontwikkelingen en onvoorziene cookies</p>
-        <p>De teksten van onze website kunnen op ieder moment worden aangepast door voortdurende ontwikkelingen. Dit geldt ook voor onze cookieverklaring. Neem deze verklaring daarom regelmatig door om op de hoogte te blijven van eventuele wijzigingen.</p>
-        <p>In blogartikelen kan gebruik worden gemaakt van content die op andere sites wordt gehost en op www.cafetariadesluis.nl wordt ontsloten door middel van bepaalde codes (embedded content). Denk hierbij aan bijvoorbeeld YouTube video's. Deze codes maken vaak gebruik van cookies. Wij hebben echter geen controle op wat deze derde partijen met hun cookies doen.</p>
-        <p>Het kan ook voorkomen dat via onze websites cookies worden geplaatst door anderen, waarvan wijzelf niet altijd op de hoogte zijn. Kom je op onze website onvoorziene cookies tegen die je niet kunt terugvinden in ons overzicht? Laat het ons weten via info@cafetariadesluis.nl. Je kan ook rechtstreeks contact opnemen met de derde partij en vraag welke cookies ze plaatsten, wat de reden daarvoor is, wat de levensduur van de cookie is en op welke manier ze je privacy gewaarborgd hebben.</p>
-        <p className="font-semibold my-2 text-green">7. Slotopmerkingen</p>
-        <p>Wij zullen deze verklaringen af en toe aan moeten passen, bijvoorbeeld wanneer we onze website aanpassen of de regels rondom cookies wijzigen. Je kunt deze webpagina raadplegen voor de laatste versie.</p>
-        <p>Mocht je nog vragen en/of opmerkingen hebben neem dan contact op met info@cafetariadesluis.nl</p>
-        <p>Deze cookieverklaring is gemaakt met behulp van RocketLawyer (https://www.rocketlawyer.com/nl/nl)</p>
-        <p>01 december 2022</p>
-      </div>
+					Het gebruik van cookies is van groot belang voor het goed laten draaien van onze website, maar ook cookies
+					waarvan je niet direct het effect ziet zijn zeer belangrijk. Dankzij de (anonieme) input van bezoekers kunnen
+					we het gebruik van de website verbeteren en deze gebruiksvriendelijker maken.
+				</p>
+				<p className='font-semibold my-2 text-green'>2. Toestemming voor het gebruik van cookies</p>
+				<p>
+					Voor het gebruik van bepaalde cookies is jouw toestemming vereist. Voor de cookies die wij gebruiken is geen
+					toestemming vereist.{" "}
+				</p>
+				<p className='font-semibold my-2 text-green'>3. Type cookies en hun doelstellingen</p>
+				<p>Wij gebruiken de volgende type cookies:</p>
+				<ul>
+					<li>
+						Functionele cookies: hiermee kunnen we de website beter laten functioneren en is die gebruiksvriendelijker
+						voor de bezoeker. Bijvoorbeeld: we slaan je inloggegevens op of wat je in je winkelmandje hebt gestopt.{" "}
+					</li>
+				</ul>
+				<p className='font-semibold my-2 text-green'>4. Je rechten met betrekking tot je gegevens</p>
+				<p>
+					Je hebt het recht op inzage, rectificatie, beperking en verwijdering van persoonsgegevens. Daarnaast heb je
+					recht van bezwaar tegen verwerking van persoonsgegevens en recht op gegevensoverdraagbaarheid. Je kunt deze
+					rechten uitoefenen door ons een mail te sturen via info@cafetariadesluis.nl.
+				</p>
+				<p>
+					Om misbruik te voorkomen kunnen wij je daarbij vragen om je adequaat te identificeren. Wanneer het gaat om
+					inzage in persoonsgegevens gekoppeld aan een cookie, vragen we je een kopie van het cookie in kwestie mee te
+					sturen. Je kunt deze terugvinden in de instellingen van je browser.
+				</p>
+				<p className='font-semibold my-2 text-green'>5. Cookies blokkeren en verwijderen</p>
+				<p>
+					Je kunt cookies te allen tijde eenvoudig zelf blokkeren en verwijderen via je internetbrowser. Ook kun je je
+					internetbrowser zodanig instellen dat je een bericht ontvangt als er een cookie wordt geplaatst. Je kunt ook
+					aangeven dat bepaalde cookies niet geplaatst mogen worden. Bekijk voor deze mogelijkheid de helpfunctie van je
+					browser. Als je de cookies in je browser verwijdert, kan dat gevolgen hebben voor het prettige gebruik van
+					deze website.
+				</p>
+				<p>
+					Sommige tracking cookies worden geplaatst door derden die onder meer via onze website advertenties aan je
+					vertonen. Deze cookies kan je centraal verwijderen via youronlinechoices.com
+				</p>
+				<p>
+					Wees je er wel van bewust dat als je geen cookies wilt, wij niet meer kunnen garanderen dat onze Website
+					helemaal goed werkt. Het kan zijn dat enkele functies van de site verloren gaan of zelfs dat je de website
+					helemaal niet meer kunt bezoeken. Daarnaast betekent het weigeren van cookies ook niet dat je helemaal geen
+					advertenties meer te zien krijgt. De advertenties zijn dan alleen niet meer toegesneden op jouw interesses en
+					kun daardoor vaker worden herhaald.
+				</p>
+				<p>
+					Hoe je je instellingen kunt aanpassen, verschilt per browser. Raadpleeg indien nodig de helpfunctie van jouw
+					browser, of klik op één van de onderstaande links om direct naar de handleiding van je browser te gaan.
+				</p>
+				<p className='font-semibold my-2 text-green'>6. Nieuwe ontwikkelingen en onvoorziene cookies</p>
+				<p>
+					De teksten van onze website kunnen op ieder moment worden aangepast door voortdurende ontwikkelingen. Dit
+					geldt ook voor onze cookieverklaring. Neem deze verklaring daarom regelmatig door om op de hoogte te blijven
+					van eventuele wijzigingen.
+				</p>
+				<p>
+					In blogartikelen kan gebruik worden gemaakt van content die op andere sites wordt gehost en op
+					www.cafetariadesluis.nl wordt ontsloten door middel van bepaalde codes (embedded content). Denk hierbij aan
+					bijvoorbeeld YouTube video's. Deze codes maken vaak gebruik van cookies. Wij hebben echter geen controle op
+					wat deze derde partijen met hun cookies doen.
+				</p>
+				<p>
+					Het kan ook voorkomen dat via onze websites cookies worden geplaatst door anderen, waarvan wijzelf niet altijd
+					op de hoogte zijn. Kom je op onze website onvoorziene cookies tegen die je niet kunt terugvinden in ons
+					overzicht? Laat het ons weten via info@cafetariadesluis.nl. Je kan ook rechtstreeks contact opnemen met de
+					derde partij en vraag welke cookies ze plaatsten, wat de reden daarvoor is, wat de levensduur van de cookie is
+					en op welke manier ze je privacy gewaarborgd hebben.
+				</p>
+				<p className='font-semibold my-2 text-green'>7. Slotopmerkingen</p>
+				<p>
+					Wij zullen deze verklaringen af en toe aan moeten passen, bijvoorbeeld wanneer we onze website aanpassen of de
+					regels rondom cookies wijzigen. Je kunt deze webpagina raadplegen voor de laatste versie.
+				</p>
+				<p>Mocht je nog vragen en/of opmerkingen hebben neem dan contact op met info@cafetariadesluis.nl</p>
+				<p>Deze cookieverklaring is gemaakt met behulp van RocketLawyer (https://www.rocketlawyer.com/nl/nl)</p>
+				<p>01 december 2022</p>
+			</div>
   );
 }
