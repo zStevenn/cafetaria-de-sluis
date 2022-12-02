5ad06e39fc9fe699694210fab95e9fdc53b63938
Steven <56149788+zStevenn@users.noreply.github.com>
2022-12-02 14:53:33 +0100

    Update PrivacyPolicy.js

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,7 +1,128 @@
 export default function PrivacyPolicy() {
 	return (
-		<>
-			<h1>Privacy Policy</h1>
-		</>
+		<div className="flex flex-col flex-nowrap w-10/12 sm:w-8/12 m-auto">
+				<h2 className="text-3xl font-semibold my-4 text-green">
+					Privacy Policy
+				</h2>
+				<p>
+					Privacy policy voor Cafetaria de Sluis, eigenaar van www.cafetariadesluis.nl
+				</p>
+				<p className="font-semibold my-2 text-green">
+					1. Waarborgen Privacy
+				</p>
+				<p>
+					Het waarborgen van de privacy van bezoekers van www.cafetariadesluis.nl is een belangrijke taak voor ons.
+					Daarom beschrijven we in onze privacy policy welke informatie we verzamelen en hoe we deze informatie
+					gebruiken.
+				</p>
+				<p className="font-semibold my-2 text-green">
+					2. Toestemming
+				</p>
+				<p>
+					Door de informatie en de diensten op www.cafetariadesluis.nl te gebruiken, gaat u akkoord met onze privacy
+					policy en de voorwaarden die wij hierin hebben opgenomen.
+				</p>
+				<p className="font-semibold my-2 text-green">
+					3. Vragen
+				</p>
+				<p>
+					Als u meer informatie wilt ontvangen, of vragen hebt over de privacy policy van Cafetaria de Sluis en
+					specifiek www.cafetariadesluis.nl, kun u ons benaderen via e-mail. Ons e-mailadres is
+					info@cafetariadesluis.nl.
+				</p>
+				<p className="font-semibold my-2 text-green">
+					4. Monitoren gedrag bezoeker
+				</p>
+				<p>
+					www.cafetariadesluis.nl maakt gebruik van verschillende technieken om bij te houden wie de website bezoekt,
+					hoe deze bezoeker zich op de website gedraagt en welke pagina’s worden bezocht. Dat is een gebruikelijke
+					manier van werken voor websites omdat het informatie oplevert op die bijdraagt aan de kwaliteit van de
+					gebruikerservaring. De informatie die we, via cookies, registreren, bestaat uit onder meer IP-adressen, het
+					type browser en de bezochte pagina’s.
+				</p>
+				<p>
+					Tevens monitoren we waar bezoekers de website voor het eerst bezoeken en vanaf welke pagina ze vertrekken.
+					Deze informatie houden we anoniem bij en is niet gekoppeld aan andere persoonlijke informatie,
+					<a href='https://www.hp-links.com/' target='_blank'>
+						link
+					</a>
+					.
+				</p>
+				<p className="font-semibold my-2 text-green">
+					5. Gebruik van cookies
+				</p>
+				<p>
+					www.cafetariadesluis.nl plaatst cookies bij bezoekers. Dat doen we om informatie te verzamelen over de
+					pagina’s die gebruikers op onze website bezoeken, om bij te houden hoe vaak bezoekers terug komen en om te
+					zien welke pagina’s het goed doen op de website. Ook houden we bij welke informatie de browser deelt.
+				</p>
+				<p className="font-semibold my-2 text-green">
+					6. Cookies uitschakelen
+				</p>
+				<p>
+					U kunt er voor kiezen om cookies uit te schakelen. Dat doet u door gebruik te maken de mogelijkheden van uw
+					browser. U vindt meer informatie over deze mogelijkheden op de website van de aanbieder van uw browser.
+				</p>
+				<p className="font-semibold my-2 text-green">
+					7. Cookies van derde partijen
+				</p>
+				<p>
+					Het is mogelijk dat derde partijen, zoals Google, op onze website adverteren of dat wij gebruik maken van een
+					andere dienst. Daarvoor plaatsen deze derde partijen in sommige gevallen cookies. Deze cookies zijn niet door
+					www.cafetariadesluis.nl te beïnvloeden.
+				</p>
+				<p className="font-semibold my-2 text-green">
+					8. Onze adverteerders
+				</p>
+				<p>Op www.cafetariadesluis.nl adverteren meerdere partijen, die allemaal hun eigen cookies plaatsen.</p>
+				<p>
+					Deze adverteerders gebruiken technologie voor hun advertenties en de links in hun advertenties die direct door
+					uw browser worden herkend. Ze gebruiken onder meer automatische herkenning van IP-adressen die rechtstreeks
+					naar de adverteerder wordt gestuurd.
+				</p>
+				<p>
+					Daarnaast worden cookies, javascript en eventueel webbeacons ingezet om te monitoren hoe effectief de
+					campagnes van deze partijen zijn. www.cafetariadesluis.nl heeft noch invloed noch controle over de cookies,
+					het javascript en de webbeacons die onze adverteerders gebruiken.
+				</p>
+				<p>
+					Al deze partijen hebben een eigen privacy policy en hanteren deze privacy policy voor het gebruik van hun
+					website en de daaraan gekoppelde diensten. Meer informatie over hoe deze partijen omgaan met privacy vindt u
+					op de websites van deze partijen.
+				</p>
+				<p className="font-semibold my-2 text-green">
+					9. Privacy policy van adverteerders/derde partijen
+				</p>
+				<p>
+					Voor meer informatie over de privacy policy van onze adverteerders en derde partijen die verbonden zijn aan
+					deze website, kun u terecht op de websites van deze respectievelijke partijen. www.cafetariadesluis.nl kan
+					geen invloed uitoefenen op deze cookies en de privacy policy van door derden geplaatste cookies. Deze cookies
+					vallen buiten het bereik van de privacy policy van www.cafetariadesluis.nl.
+				</p>
+				<p className="font-semibold my-2 text-green">
+					10. DART-cookie van Google
+				</p>
+				<p>
+					Het gebruik van een DART-cookie door Google, maakt het mogelijk dat er advertenties op onze website worden
+					getoond. Daarnaast wordt deze cookie gebruikt voor het tonen van advertenties op andere websites. U kunt er
+					voor kiezen om geen gebruik meer te maken van de DART Cookie. Dat doet u door de volgende website te bezoeken:
+					<a href='https://support.google.com/adsense/answer/2839090?hl=nl' target='_blank' rel='noopener noreferrer'>
+						DART
+					</a>
+				</p>
+				<p className="font-semibold my-2 text-green">
+					11. Informatie van kinderen
+				</p>
+				<p>
+					Wij vinden het belangrijk om te zorgen voor extra bescherming voor kinderen die online actief zijn. We
+					moedigen ouders en opvoeders aan om samen tijd online te besteden. Op die manier kunt u zien hoe uw kinderen
+					zich online gedragen, wat ze precies online doen en kunt u helpen in het sturen van hun gedrag.
+				</p>
+				<p>
+					www.cafetariadesluis.nl verzamelt geen informatie van kinderen jonger dan 13 jaar oud. Mocht u constateren dat
+					deze regel wordt overtreden door ons of ons team, neemt u dan contact met ons op via bovenstaande
+					contactmogelijkheid.
+				</p>
+			</div>
 	);
 }
