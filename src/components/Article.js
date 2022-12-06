03cfb4be8796c65614c3b00a593b5e71ff2994d7
Steven <56149788+zStevenn@users.noreply.github.com>
2022-12-06 16:39:35 +0100

    Adds ArticleList component
    ArticleList renders the ArticleWithImage photo

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,4 @@
-export function ArticleWithImage(props) {
+function ArticleWithImage(props) {
   return (
     <div className="flex flex-row m-4 max-h-36 h-36 justify-center items-center">
       {/* Image */}
@@ -10,6 +10,31 @@ export function ArticleWithImage(props) {
         <p className="text-lg font-medium h-6 mb-1">{props.textTitle}</p>
         <p className="text-base font-base text-clip overflow-hidden h-24 max-w-prose">{props.textDescription}</p>
       </div>
+    </div>
+  );
+}
+
+export function ArticleList() {
+  return (
+    <div className="flex flex-col m-2">
+      <ArticleWithImage 
+      imgSrc={"https://loremflickr.com/cache/resized/65535_51722617146_5156e310d5_300_300_nofilter.jpg"} 
+      imgTitle={"placeholder"} 
+      title={"Artikel 1"} 
+      text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dui sapien Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dui sapien Curabitur dui sapien Lorem ipsum dolor sit ametn"}
+      />
+      <ArticleWithImage 
+      imgSrc={"https://loremflickr.com/cache/resized/65535_51722617146_5156e310d5_300_300_nofilter.jpg"} 
+      imgTitle={"placeholder"} 
+      title={"Artikel 1"} 
+      text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dui sapien Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dui sapien Curabitur dui sapien Lorem ipsum dolor sit ametn"}
+      />
+      <ArticleWithImage 
+      imgSrc={"https://loremflickr.com/cache/resized/65535_51722617146_5156e310d5_300_300_nofilter.jpg"} 
+      imgTitle={"placeholder"} 
+      title={"Artikel 1"} 
+      text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dui sapien Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dui sapien Curabitur dui sapien Lorem ipsum dolor sit ametn"}
+      />
     </div>
   );
 }
