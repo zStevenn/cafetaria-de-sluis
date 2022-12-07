512e08d309f56a632b08a6733224351856c98694
zStevenn <steventje678@hotmail.com>
2022-12-07 22:50:49 +0100

    Refactors colors for primarybutton

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 export function PrimaryButton(props) {
   return(
     <a href={props.to}>
-      <button className="bg-[#430D47] hover:bg-[#430D47]/60 text-white font-bold py-2 px-4 border-2 border-[#430D47] rounded-full">
+      <button className="bg-secondary hover:bg-secondary/60 text-white font-bold py-2 px-4 border-2 border-secondary rounded-full">
         {props.children}
       </button>
     </a>
