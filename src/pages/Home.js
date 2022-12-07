21f469b62857d030b29b614f632d40b17c8d77a9
zStevenn <steventje678@hotmail.com>
2022-12-07 23:37:28 +0100

    Refactors herosection using primary buttons and hero-image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
-import HeroSection from "../components/content/HeroSection";
 import CTASection from "../components/content/CTASection";
 import ProductHighlights from "../components/content/ProductHighlights";
 import Review from "../components/content/Review";
+import Hero from "../components/Hero";
 
 export default function Home() {
 	return (
 		<>
-			<HeroSection />
+			<Hero />
 			<CTASection />
 			<ProductHighlights />
 			<Review />
