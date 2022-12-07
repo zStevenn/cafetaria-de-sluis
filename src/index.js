f1da9d03b912c29096bf6cc845f595e3713f2a24
zStevenn <steventje678@hotmail.com>
2022-12-07 22:42:11 +0100

    Added font import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import "@fontsource/outfit"; // Defaults to weight 400.
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
