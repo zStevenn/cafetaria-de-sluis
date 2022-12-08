15fb7f5104138ca15d1ed61221ee30abe309f328
Steven <56149788+zStevenn@users.noreply.github.com>
2022-12-08 16:59:02 +0100

    imports allergymenu, refactors responsiveness

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,29 +1,33 @@
-import { MenuItem, MenuSubheader } from "./MenuComponents";
+import { AllergyMenu, MenuItem, MenuSubheader } from "./MenuComponents";
 
 export default function Menu() {
   return (
-    <div className="flex flex-col flex-nowrap justify-start">
-      <p className="text-white text-2xl font-bold text-center m-3">Menukaart</p>
-      <div className="text-white mx-6">
-        <MenuSubheader name={"Menu header 1"}/>
-        <MenuItem name={"Menu item 1"} price={"9.95"}/>
-        <MenuItem name={"Menu item 1"} price={"9.95"}/>
-        <MenuItem name={"Menu item 1"} price={"9.95"}/>
-        <MenuItem name={"Menu item 1"} price={"9.95"}/>
-      </div>
-      <div className="text-white mx-6">
-        <MenuSubheader name={"Menu header 2"}/>
-        <MenuItem name={"Menu item 3"} price={"9.95"}/>
-        <MenuItem name={"Menu item 3"} price={"9.95"}/>
-        <MenuItem name={"Menu item 3"} price={"9.95"}/>
-        <MenuItem name={"Menu item 3"} price={"9.95"}/>
-      </div>
-      <div className="text-white mx-6">
-        <MenuSubheader name={"Menu header 2"}/>
-        <MenuItem name={"Menu item 5"} price={"9.95"}/>
-        <MenuItem name={"Menu item 6"} price={"9.95"}/>
-        <MenuItem name={"Menu item 8"} price={"9.95"}/>
-        <MenuItem name={"Menu item 74"} price={"9.95"}/>
+    <div className="flex flex-row justify-start">
+      {/* Desktop allergy slide */}
+      <AllergyMenu />
+      <div className="flex flex-col flex-nowrap basis-full md:basis-3/5">
+        <p className="text-white text-2xl md:text-3xl font-bold text-center m-3 md:mt-10">Menukaart</p>
+        <div className="text-white mx-6">
+          <MenuSubheader name={"Menu header 1"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+        </div>
+        <div className="text-white mx-6">
+          <MenuSubheader name={"Menu header 1"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+        </div>
+        <div className="text-white mx-6">
+          <MenuSubheader name={"Menu header 1"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+          <MenuItem name={"Menu item 1"} price={"9.95"}/>
+        </div>
       </div>
     </div>
   );
