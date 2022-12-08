f82406fc2d11b5ae960e39cc5d1a848085b8552a
Steven <56149788+zStevenn@users.noreply.github.com>
2022-12-08 16:58:20 +0100

    Creates allergy menu for desktop

--- src/components/Menu/MenuComponents.js
+++ src/components/Menu/MenuComponents.js
@@ -6,11 +6,11 @@ export function MenuSubheader(props) {
 
 export function MenuItem(props) {
   return(
-    <div className="flex flex-row justify-between flex-nowrap">
+    <div className="flex flex-row justify-between sm:justify-start flex-nowrap items-center md:justify-center">
       {/* Item name */}
-      <p>{props.name}</p>
+      <p className="sm:basis-1/4 md:basis-1/3">{props.name}</p>
       {/* Allergy icons */}
-      <div className="flex flex-row justify-evenly flex-nowrap">
+      <div className="flex flex-row sm:basis-2/4 md:basis-1/3 justify-evenly sm:justify-center flex-nowrap">
         <span>*</span>
         <span>*</span>
         <span>*</span>
@@ -18,7 +18,36 @@ export function MenuItem(props) {
         <span>*</span>
       </div>
       {/* Price */}
-      <p>&euro; {props.price}</p>
+      <p className="sm:basis-1/4 md:basis-1/3 md:text-center">&euro; {props.price}</p>
+    </div>
+  );
+}
+
+export function AllergyMenu() {
+  return(
+    <div className="hidden md:flex md:flex-col md:justify-center md:items-center">
+      <div className="md:p-6 md:bg-purple-900 md:basis-1/5 md:m-4 md:rounded-xl">
+        <div className="flex flex-row flex-nowrap justify-between p-2">
+          <span>icon</span>
+          <span>Text</span>
+        </div>
+        <div className="flex flex-row flex-nowrap justify-between p-2">
+          <span>icon</span>
+          <span>Text</span>
+        </div>
+        <div className="flex flex-row flex-nowrap justify-between p-2">
+          <span>icon</span>
+          <span>Text</span>
+        </div>
+        <div className="flex flex-row flex-nowrap justify-between p-2">
+          <span>icon</span>
+          <span>Text</span>
+        </div>
+        <div className="flex flex-row flex-nowrap justify-between p-2">
+          <span>icon</span>
+          <span>Text</span>
+        </div>
+      </div>
     </div>
   );
 }
