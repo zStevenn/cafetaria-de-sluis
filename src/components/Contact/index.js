3f55c911af14440f6520f1bea39ccc4ec5672e51
Steven <56149788+zStevenn@users.noreply.github.com>
2022-12-09 10:43:36 +0100

    Create export Contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,1 +1,11 @@
+import { ContactUs, ContactForm } from "./ContactComponents";
 
+export default function Contact() {
+  return (
+		<>
+			<p className='text-white text-lg font-semibold text-center m-4'>Contacteer ons!</p>
+			<ContactUs />
+			<ContactForm />
+		</>
+	);
+}
