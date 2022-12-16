e7d7c9aba8336533f7a2025b0d34eac7e3e4c6f6
zStevenn <steventje678@hotmail.com>
2022-12-16 10:53:15 +0100

    Refactors reviewlist with in-line colors

--- src/components/CustomerReview.js
+++ src/components/CustomerReview.js
@@ -1,5 +1,5 @@
 import { BsFillArrowLeftCircleFill as ArrowLeft, BsFillArrowRightCircleFill as ArrowRight } from "react-icons/bs";
-import { BsStarFill, BsStarHalf, BsStar  } from "react-icons/bs";
+import { BsStarFill } from "react-icons/bs";
 
 function ReviewCard(props) {
 	const stars = [];
@@ -7,8 +7,8 @@ function ReviewCard(props) {
 		stars.push(<BsStarFill/>);
 	}
 	return (
-		<div className="flex flex-col flex-nowrap justify-center items-center bg-green-900 rounded-xl px-6 py-3 m-2 md:px-10 md:py-5">
-			<div className="text-white text-base md:text-lg font-base md:font-lg truncate">{props.name}</div>
+		<div className="flex flex-col flex-nowrap justify-center items-center bg-white rounded-xl px-6 py-3 m-2 md:px-10 md:py-5">
+			<div className="text-primary text-base font-semibold md:text-lg font-base md:font-lg truncate">{props.name}</div>
 			<div className="flex flex-row flex-nowrap p-2 text-yellow-500">{stars}</div>
 		</div>
 	);
@@ -16,15 +16,13 @@ function ReviewCard(props) {
 
 export default function ReviewList() {
 	return (
-		<div className="flex flex-col justify-center items-center bg-white">
-			<div className="">
-				<p className="text-3xl font-bold m-2">Reviews</p>
-			</div>
+		<div className="flex flex-col justify-center items-center bg-primary">
+			<p className="text-3xl text-white font-bold m-2 ">Reviews</p>
 			<div className="flex flex-row flex-nowrap justify-center items-center">
-			<ArrowLeft className="text-3xl fill-primary" />
-			<ReviewCard name={"Steven Li"} rating={2} />
-			<ReviewCard name={"Superman"} rating={4} />
-			<ArrowRight className="text-3xl fill-primary" />
+				<ArrowLeft className="text-3xl fill-white" />
+				<ReviewCard name={"Steven Li"} rating={4} />
+				<ReviewCard name={"Ai Hua Sun"} rating={4} />
+				<ArrowRight className="text-3xl fill-white" />
 			</div>
 		</div>
 	);
