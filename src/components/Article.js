17f808be99b4d7aa22b9bd4849701544bb57f1d2
zStevenn <steventje678@hotmail.com>
2022-12-16 10:46:45 +0100

    Refactors colors of article section

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,7 +6,7 @@ function ArticleWithImage(props) {
 				<img className="rounded-xl object-cover max-w-full" src={props.imgSrc} alt={props.imgTitle} />
 			</div>
 			{/* Text */}
-			<div className="flex flex-col justify-center ml-2 h-40 basis-1/2 sm:basis-2/3 md:basis-3/4 lg:basis-1/3">
+			<div className="flex flex-col justify-center text-white ml-2 h-40 basis-1/2 sm:basis-2/3 md:basis-3/4 lg:basis-1/3">
 				<p className="text-lg font-medium h-6 mb-1">{props.title}</p>
 				<p className="text-base font-base text-clip overflow-hidden h-24 max-w-prose">{props.text}</p>
 			</div>
@@ -16,7 +16,7 @@ function ArticleWithImage(props) {
 
 export default function ArticleList() {
 	return (
-		<div className="flex flex-col py-6">
+		<div className="flex flex-col py-6 bg-primary text-white">
 			<p className="text-center text-3xl font-semibold">Recent</p>
 			<ArticleWithImage
 				imgSrc={"https://loremflickr.com/cache/resized/65535_51722617146_5156e310d5_300_300_nofilter.jpg"}
