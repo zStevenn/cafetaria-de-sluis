4601c353cd1ba2496285ff17fded6c0768cda160
zStevenn <steventje678@hotmail.com>
2022-12-21 17:17:19 +0100

    Refactor to category component responsiveness

--- src/components/Hero/HeroComponents.js
+++ src/components/Hero/HeroComponents.js
@@ -7,8 +7,8 @@ export function Category() {
 	return (
 		<div className="flex flex-col bg-primary text-white fill-white">
 			<Title text={"Heerlijke gerechten"} className="my-10" />
-			<ul className="flex flex-row justify-evenly mb-10">
-				<li className="flex justify-center items-center content-center group">
+			<ul className="flex flex-col sm:flex-row justify-evenly mb-10">
+				<li className="flex justify-center items-center content-center group my-6 sm:my-0">
 					<a
 						className="flex flex-col items-center group-hover:scale-110 transition-all"
 						href="https://www.e-food.nl/driebergen/cafeteria-de-sluis/burgers/sluis-burgers"
@@ -17,13 +17,13 @@ export function Category() {
 						<Subtitle text={"Sluisburgers"} />
 					</a>
 				</li>
-				<li className="flex justify-center items-center content-center group">
+				<li className="flex justify-center items-center content-center group my-6 sm:my-0">
 					<a className="flex flex-col items-center group-hover:scale-110 transition-all" href="https://www.e-food.nl/driebergen/cafeteria-de-sluis/frites">
 						<GiFrenchFries className="text-4xl" />
 						<Subtitle text={"Frietjes"} />
 					</a>
 				</li>
-				<li className="flex justify-center items-center content-center group">
+				<li className="flex justify-center items-center content-center group my-6 sm:my-0">
 					<a
 						className="flex flex-col items-center group-hover:scale-110 transition-all"
 						href="https://www.e-food.nl/driebergen/cafeteria-de-sluis/ijs--milkshakes/milkshakes"
