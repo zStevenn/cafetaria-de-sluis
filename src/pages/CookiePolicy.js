9f5c2a002620eefd5b0538cd075e671b8551c830
zStevenn <steventje678@hotmail.com>
2023-01-01 22:48:40 +0100

    refactors to cookie policy text

--- src/pages/CookiePolicy.js
+++ src/pages/CookiePolicy.js
@@ -1,23 +1,38 @@
 const CookiePolicy = () => {
 	return (
-		<div className="px-10 pt-6 py-4 ">
-			<h1 className="text-3xl mb-4 font-bold">Cookiesbeleid</h1>
+		<div className="px-10 pt-6 py-4 min-h-[75vh]">
+			<h1 className="text-3xl font-bold mb-2">Cookiesbeleid</h1>
+			<h3 className="mb-4 text-lg">
+				Ons cookiebeleid legt uit hoe we cookies gebruiken op onze website.
+			</h3>
+			<p className="font-semibold">Wat zijn cookies?</p>
 			<p className="mb-4">
-				Onze website maakt gebruik van cookies om uw gebruikservaring te verbeteren. Een cookie is een klein
-				tekstbestand dat op uw computer, tablet of smartphone wordt opgeslagen wanneer u onze website bezoekt.
+				Cookies zijn kleine tekstbestanden die door een website op uw computer of mobiele apparaat worden opgeslagen
+				wanneer u de site bezoekt. Cookies worden gebruikt om informatie op te slaan die nodig is voor het goed
+				functioneren van de website, zoals uw voorkeuren en instellingen.
 			</p>
+			<p className="font-semibold">Welke cookies gebruiken we?</p>
 			<p className="mb-4">
-				We gebruiken functionele en analytische cookies om uw bezoek aan onze website gemakkelijker te maken en om te
-				begrijpen hoe bezoekers onze website gebruiken. Deze cookies worden alleen gebruikt voor interne doeleinden en
-				worden niet aan derden verstrekt.
+				We gebruiken cookies om onze website te verbeteren en aan te passen aan de behoeften van onze gebruikers.
+				Bijvoorbeeld, we gebruiken cookies om uw taalvoorkeuren op te slaan zodat we de website in uw gewenste taal
+				kunnen weergeven. We gebruiken ook cookies om onze website te analyseren en te meten, zodat we kunnen zien hoe
+				de site wordt gebruikt en hoe we deze kunnen verbeteren.
 			</p>
+			<p className="font-semibold">Kunt u cookies uitschakelen?</p>
 			<p className="mb-4">
-				We gebruiken ook tracking cookies van derden om u relevante advertenties te tonen op andere websites. U kunt
-				deze cookies uitschakelen door uw browserinstellingen te wijzigen.
+				Ja, u kunt cookies uitschakelen via de instellingen van uw internetbrowser. Houd er rekening mee dat als u
+				cookies uitschakelt, sommige delen van onze website mogelijk niet correct zullen werken.
 			</p>
-			<p>
-				Als u meer wilt weten over het gebruik van cookies op onze website, of als u wilt weten hoe u cookies kunt
-				beheren of verwijderen, kunt u ons privacybeleid raadplegen.
+			<p className="font-semibold">Wijzigingen aan ons cookiebeleid</p>
+			<p className="mb-4">
+				We behouden ons het recht voor om dit cookiebeleid op elk moment te wijzigen. Het is uw verantwoordelijkheid om
+				ons cookiebeleid regelmatig te raadplegen om op de hoogte te blijven van eventuele wijzigingen. Als u vragen
+				heeft over ons cookiebeleid of hoe we cookies gebruiken op onze website, neem dan gerust contact met ons op.
+			</p>
+			<p className="font-semibold">Meer informatie</p>
+			<p className="mb-4">
+				Voor meer informatie over cookies, inclusief hoe u ze kunt beheren en verwijderen, bezoekt u
+				www.allaboutcookies.org.
 			</p>
 		</div>
 	);
