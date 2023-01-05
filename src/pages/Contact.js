e087e6e7bb73e58d32dd08a030ab2a4b9f28424b
zStevenn <steventje678@hotmail.com>
2023-01-05 16:06:48 +0100

    Adds map to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,16 @@
 import { Title } from "../components/Text";
 import { ContactUs, ContactForm } from "../components/Contact";
+import Map from "../components/Map";
 
 export default function Contact() {
-  return (
-    <div className="bg-primary">
+	return (
+		<div className="bg-primary flex flex-col">
 			<Title text={"Contacteer ons!"} className="py-4" />
 			<ContactUs />
-			<ContactForm />
+			<div className="flex flex-col md:flex-row md:justify-evenly py-8">
+        <Map />
+				<ContactForm />
+			</div>
 		</div>
-  );
+	);
 }
