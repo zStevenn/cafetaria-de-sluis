519f5ebaa0179d0b99a3ee7415a36904cd35b393
zStevenn <steventje678@hotmail.com>
2023-01-05 20:43:45 +0100

    Added responsiveness to newscard on mobile

--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,9 @@
 export function NewsCard(props) {
 	return (
-		<div className="bg-white p-6 mb-6 rounded-lg shadow-lg w-96 h-80">
+		<div className="bg-white p-6 rounded-lg shadow-lg w-full sm:w-1/2">
 			<h1 className="text-xl font-bold">{props.title}</h1>
 			<h3 className="text-base mb-2 text-slate-700">{props.date}</h3>
-			<p className="text-clip">
+			<p className="overflow-hidden min-h-fit">
 				{props.text}
 			</p>
 		</div>
