4f7691260ed4746c4f84a42776a513e1c9950c89
zStevenn <steventje678@hotmail.com>
2023-01-05 15:06:23 +0100

    Adds images instead of placeholders

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,13 @@
 import { Title } from "./Text";
+import ArticleImg01 from "../assets/img/static/article01.jpg";
+import ArticleImg02 from "../assets/img/static/article02.jpg";
+import ArticleImg03 from "../assets/img/static/article03.jpg";
 
 function ArticleWithImage(props) {
 	return (
 		<div className="flex flex-row my-4 mx-6 justify-start lg:justify-center gap-4">
 			{/* Image */}
-			<img className="rounded-xl object-cover w-32 md:w-40" src={props.imgSrc} alt={props.imgTitle} />
+			<img className="rounded-xl object-cover w-32 h-32 md:w-40 md:h-40 border-white border shadow-sm shadow-white" src={props.imgSrc} alt={props.imgTitle} />
 			{/* Text */}
 			<div className="flex flex-col justify-center text-white ml-2 max-h-32">
 				<p className="text-lg font-medium mb-1">{props.title}</p>
@@ -19,7 +22,7 @@ export default function ArticleList() {
 		<div className="flex flex-col py-6 bg-primary text-white">
 			<Title text={"Recente activiteiten"} className="my-10" />
 			<ArticleWithImage
-				imgSrc={"https://via.placeholder.com/1000"}
+				imgSrc={ArticleImg01}
 				imgTitle={"Nieuwe website"}
 				title={"Nieuwe website"}
 				text={
@@ -27,7 +30,7 @@ export default function ArticleList() {
 				}
 			/>
 			<ArticleWithImage
-				imgSrc={"https://via.placeholder.com/1000"}
+				imgSrc={ArticleImg03}
 				imgTitle={"Tegels vervangen"}
 				title={"Tegels vervangen"}
 				text={
@@ -35,11 +38,11 @@ export default function ArticleList() {
 				}
 			/>
 			<ArticleWithImage
-				imgSrc={"https://via.placeholder.com/1000"}
+				imgSrc={ArticleImg02}
 				imgTitle={"placeholder"}
-				title={"Prijsverhoging"}
+				title={"Gelukkig nieuwjaar!"}
 				text={
-					"Helaas hebben wij moeten besluiten om onze prijzen te verhogen. Dit komt door de hoge kosten van de grondstoffen. We hopen dat u dit begrijpt. We zullen ons best doen om de prijsverhoging zo klein mogelijk te houden."
+					"Gelukkig nieuwjaar! We hopen dat 2023 een mooi jaar wordt voor iedereen. We zijn erg dankbaar voor de steun die we hebben gekregen van onze klanten en we hopen dat we u in 2023 nog beter van dienst kunnen zijn."
 				}
 			/>
 		</div>
