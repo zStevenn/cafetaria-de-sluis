42aaebd142b4eeded1d29f35779cf48d4c767688
zStevenn <steventje678@hotmail.com>
2023-01-06 16:00:21 +0100

    Added lazy loading to google maps iframe

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,20 @@
 import { Title } from "../components/Text";
 import { ContactUs, ContactForm } from "../components/Contact";
-import Map from "../components/Map";
+import { lazy, Suspense } from "react";
+import { AiOutlineLoading } from "react-icons/ai";
+
+// Lazy load the map
+const Map = lazy(() => import("../components/Map"));
 
 export default function Contact() {
 	return (
 		<div className="bg-primary flex flex-col">
 			<Title text={"Contacteer ons!"} className="py-4" />
 			<div className="flex flex-col md:flex-row justify-center gap-4 m-8">
-				<Map className="w-full h-[34rem] p-8 md:w-1/2 mb-8 md:mb-0 md:mr-8 bg-white rounded-xl" />
 				<ContactForm className="p-8 w-full md:w-1/2" />
+				<Suspense fallback={<AiOutlineLoading className="items-center text-6xl animate-spin text-primary"/>}>
+					<Map className="w-full h-[34rem] p-8 md:w-1/2 mb-8 md:mb-0 md:mr-8 bg-white rounded-xl" />
+				</Suspense>
 			</div>
 			<ContactUs />
 		</div>
