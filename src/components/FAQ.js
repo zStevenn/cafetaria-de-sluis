17bb9ae1a035378bbd51ede6fd71a8f2195b9af1
Steven <56149788+zStevenn@users.noreply.github.com>
2023-01-10 12:26:43 +0100

    Adding react icon
    Imports KeyboardArrowDown into the button for better UI experience
    Added scaling to button to increase in size on user hover

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,10 +1,12 @@
+import { MdKeyboardArrowDown } from "react-icons/md";
+
 export function FAQ({ question, answer }) {
   const [isOpen, setIsOpen] = useState(false);
   
   return (
     <div className="px-2 py-4 bg-white text-primary rounded-lg mb-4 divide-y divide-primary">
-      <button onClick={() => setIsOpen(!isOpen)}>
-        <h6 className="font-semibold text-lg text-start">{question} IconDown</h6>
+      <button className="hover:scale-110 transition-all" onClick={() => setIsOpen(!isOpen)}>
+        <h6 className="font-semibold text-lg text-start">{question} <MdKeyboardArrowDown className="text-3xl fill-primary" /></h6>
       </button>
       {isOpen && <p className>{answer}</p>}
     </div>
