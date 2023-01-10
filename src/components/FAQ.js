fffed337c87cacb84e2bfd41fc69971e3a501adc
Steven <56149788+zStevenn@users.noreply.github.com>
2023-01-10 12:15:48 +0100

    Create FAQ Component
    This component takes in a question and answer, and displays a white button that expands when clicked upon.

---
+++ src/components/FAQ.js
@@ -0,0 +1,12 @@
+function FAQ({ question, answer }) {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  return (
+    <div className="px-2 py-4 bg-white text-primary rounded-lg mb-4 divide-y divide-primary">
+      <button onClick={() => setIsOpen(!isOpen)}>
+        <h6 className="font-semibold text-lg text-start">{question} IconDown</h6>
+      </button>
+      {isOpen && <p className>{answer}</p>}
+    </div>
+  );
+}
