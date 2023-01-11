645f0fa3debb2aa42f8a99b5d610a2e7fbcaf832
zStevenn <steventje678@hotmail.com>
2023-01-11 14:14:41 +0100

    Refactoring to footer component Extracting the openingtimes and website links to a separate function When clicking the order button, the user opens a second page to place order

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,69 @@ import { Link } from "react-router-dom";
 import { Title } from "./Text";
 import { MdOutlineArrowRightAlt } from "react-icons/md";
 
-export default function Footer() {
+// Display openingstijden
+const Openingstijden = () => {
+	const days = [
+		{ day: "Maandag", open: "Gesloten" },
+		{ day: "Dinsdag", open: "12:00 - 20:00" },
+		{ day: "Woensdag", open: "12:00 - 20:00" },
+		{ day: "Donderdag", open: "12:00 - 20:00" },
+		{ day: "Vrijdag", open: "12:00 - 20:00" },
+		{ day: "Zaterdag", open: "12:00 - 20:00" },
+		{ day: "Zondag", open: "16:00 - 20:00" },
+	];
+
+	return (
+		<div className="flex flex-col justify-start items-start text-white w-60 sm:w-auto">
+			<Title text="Openingstijden" className="text-xl font-bold py-2 tracking-wider" />
+			<ul>
+				{days.map((day) => (
+					<li className="flex flex-row hover:scale-110 transition-all">
+						<span className="w-28">{day.day}</span>
+						<span>{day.open}</span>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+// Display links to other pages
+const FooterLinks = () => {
+	const internalLinks = [
+		{ text: "Menu", link: "/menu" },
+		{ text: "Contact", link: "/contact" },
+		{ text: "Nieuws", link: "/nieuws" },
+		{ text: "FAQ", link: "/FAQ" },
+		{ text: "Privacybeleid", link: "/privacybeleid" },
+	];
+
 	const handleClick = () => {
 		window.scrollTo({ top: 0, behavior: "smooth" });
 	};
 
+	return (
+		<div className="flex flex-col justify-start items-start text-white">
+			<Title text="Informatie" className="text-xl font-bold py-2 tracking-wider" />
+			<ul>
+				<li className="hover:scale-110 transition-all">
+					<a href="https://www.e-food.nl/driebergen/cafeteria-de-sluis" target="_blank" rel="noopener noreferrer">
+						<MdOutlineArrowRightAlt className="inline" /> Bestellen
+					</a>
+				</li>
+				{internalLinks.map((link) => (
+					<li className="hover:scale-110 transition-all">
+						<Link to={link.link} onClick={handleClick}>
+							<MdOutlineArrowRightAlt className="inline" /> {link.text}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default function Footer() {
 	return (
 		<footer className="flex flex-col justify-start bg-gray-900">
 			{/* Footer link wrappers */}
@@ -14,88 +72,19 @@ export default function Footer() {
 				{/* Footer adresgegevens */}
 				<div className="flex flex-col justify-start items-start text-white">
 					<p className="text-xl font-bold py-2 tracking-wider">Adresgegevens</p>
-					<ul className="text-base">
+					<ol className="text-base">
 						<li>De Sluis 25, 3972 VC</li>
 						<li>Driebergen-Rijsenburg</li>
 						<li>Nederland</li>
 						<li>&nbsp;</li>
 						<li>(0343) - 531 646</li>
 						<li>info@cafetariadesluis.nl</li>
-					</ul>
+					</ol>
 				</div>
 				{/* Footer openingstijden */}
-				<div className="flex flex-col justify-start items-start text-white w-60 sm:w-auto">
-					<p className="text-xl font-bold py-2 tracking-wider">Openingstijden</p>
-					<div className="flex flex-row hover:scale-110 transition-all">
-						<span className="w-28">Maandag</span>
-						<span>Gesloten</span>
-					</div>
-					<div className="flex flex-row hover:scale-110 transition-all">
-						<span className="w-28">Dinsdag</span>
-						<span>12:00 - 20:00</span>
-					</div>
-					<div className="flex flex-row hover:scale-110 transition-all">
-						<span className="w-28">Woensdag</span>
-						<span>12:00 - 20:00</span>
-					</div>
-					<div className="flex flex-row hover:scale-110 transition-all">
-						<span className="w-28">Donderdag</span>
-						<span>12:00 - 20:00</span>
-					</div>
-					<div className="flex flex-row hover:scale-110 transition-all">
-						<span className="w-28">Vrijdag</span>
-						<span>12:00 - 20:00</span>
-					</div>
-					<div className="flex flex-row hover:scale-110 transition-all">
-						<span className="w-28">Zaterdag</span>
-						<span>12:00 - 20:00</span>
-					</div>
-					<div className="flex flex-row hover:scale-110 transition-all">
-						<span className="w-28">Zondag</span>
-						<span>16:00 - 20:00</span>
-					</div>
-				</div>
+				<Openingstijden />
 				{/* Footer informatie */}
-				<div className="flex flex-col justify-start items-start text-white">
-					<p className="text-xl font-bold py-2 tracking-wider">Informatie</p>
-					<ul>
-						<li className="hover:scale-110 transition-all">
-							<a href="https://www.e-food.nl/driebergen/cafeteria-de-sluis">
-								<MdOutlineArrowRightAlt className="inline" /> Bestellen
-							</a>
-						</li>
-						<li className="hover:scale-110 transition-all">
-							<Link to="/menu" onClick={handleClick}>
-								<MdOutlineArrowRightAlt className="inline" /> Menu
-							</Link>
-						</li>
-						<li className="hover:scale-110 transition-all">
-							<Link to="/contact" onClick={handleClick}>
-								<MdOutlineArrowRightAlt className="inline" /> Contact
-							</Link>
-						</li>
-						<li className="hover:scale-110 transition-all">
-							<Link to="/nieuws" onClick={handleClick}>
-								<MdOutlineArrowRightAlt className="inline" /> Nieuws
-							</Link>
-						</li>
-						<li className="hover:scale-110 transition-all">
-							<Link to="/FAQ" onClick={handleClick}>
-								<MdOutlineArrowRightAlt className="inline" /> FAQ
-							</Link>
-						</li>
-						<li className="hover:scale-110 transition-all">
-							<Link to="/privacybeleid" onClick={handleClick}>
-								<MdOutlineArrowRightAlt className="inline" /> Privacybeleid
-							</Link>
-						</li>
-						<li className="hover:scale-110 transition-all">
-							<Link to="/cookiebeleid" onClick={handleClick}>
-								<MdOutlineArrowRightAlt className="inline" /> Cookiebeleid
-							</Link>
-						</li>
-					</ul>
-				</div>
+				<FooterLinks />
 			</div>
 			{/* Footer copyright banner */}
 			<div className="bg-primary text-center py-4">
