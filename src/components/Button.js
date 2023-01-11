501ed40e4aaa6fb310a9a556f3250713116bfad8
zStevenn <steventje678@hotmail.com>
2023-01-11 14:18:51 +0100

    Added target and rel to external buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function PrimaryButton(props) {
 	return (
-		<a className="px-2 hover:scale-110 transition-all" href={props.to}>
+		<a className="px-2 hover:scale-110 transition-all" href={props.to} target="_blank" rel="noopener noreferrer">
 			<button className="truncate bg-secondary hover:bg-secondary/60 text-white font-bold py-2 px-4 border-2 border-secondary rounded-lg">
 				{props.children}
 			</button>
@@ -12,7 +12,7 @@ export function PrimaryButton(props) {
 
 export function ExternalSecondaryButton(props) {
 	return (
-		<a className="px-2 hover:scale-110 transition-all" href={props.to}>
+		<a className="px-2 hover:scale-110 transition-all" href={props.to} target="_blank" rel="noopener noreferrer">
 			<button className="truncate bg-transparent/10 hover:bg-transparent/30 text-white font-bold py-2 px-4 border-2 border-white rounded-lg">
 				{props.children}
 			</button>
