dbaad71ae69b4e1b39ff65766c8ff4bb6f3b09bc
zStevenn <steventje678@hotmail.com>
2023-01-11 13:33:00 +0100

    Changes to styling

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -2,14 +2,16 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 import { useState } from "react";
 
 export function FAQ({ question, answer }) {
-  const [isOpen, setIsOpen] = useState(false);
-  
-  return (
-    <div className="px-2 py-4 bg-white text-primary rounded-lg mb-4 divide-y divide-primary">
-      <button className="hover:scale-110 transition-all" onClick={() => setIsOpen(!isOpen)}>
-        <h6 className="font-semibold text-lg text-start">{question} <MdKeyboardArrowDown className="text-3xl fill-primary" /></h6>
-      </button>
-      {isOpen && <p className>{answer}</p>}
-    </div>
-  );
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<div className="px-2 py-4 bg-white text-primary rounded-lg mb-4 divide-y divide-primary">
+			<button onClick={() => setIsOpen(!isOpen)}>
+				<h6 className="font-semibold text-lg text-start flex flex-row flex-nowrap">
+					{question} <MdKeyboardArrowDown className="text-3xl fill-primary" />
+				</h6>
+			</button>
+			{isOpen && <p className>{answer}</p>}
+		</div>
+	);
 }
