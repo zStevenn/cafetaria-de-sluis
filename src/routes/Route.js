25acac4455a3806993cbcabb735f6ecb04adc0fe
zStevenn <steventje678@hotmail.com>
2023-01-12 23:17:06 +0100

    Extracting the routes into a separate file Introducting code splitting by using React.lazy and React.Suspense Applied lazy loading to the routes

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,32 +1,36 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Menu from "../pages/Menu";
-import Contact from "../pages/Contact";
-import News from "../pages/News";
-import Vacatures from "../pages/Vacatures";
-import NotFound from "../pages/NotFound";
-import PrivacyPolicy from "../pages/PrivacyPolicy";
-import CookiePolicy from "../pages/CookiePolicy";
-import FAQ from "../pages/FAQ";
+
+const Menu = lazy(() => import("../pages/Menu"));
+const News = lazy(() => import("../pages/News"));
+const Contact = lazy(() => import("../pages/Contact"));
+const Vacatures = lazy(() => import("../pages/Vacatures"));
+const NotFound = lazy(() => import("../pages/NotFound"));
+const PrivacyPolicy = lazy(() => import("../pages/PrivacyPolicy"));
+const CookiePolicy = lazy(() => import("../pages/CookiePolicy"));
+const FAQ = lazy(() => import("../pages/FAQ"));
 
 export default function DefaultRouter() {
 	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/menu" element={<Menu />} />
-			<Route path="/contact" element={<Contact />} />
-			<Route path="/nieuws" element={<News />} />
-			<Route path="/vacatures" element={<Vacatures />} />
-			<Route path="/privacybeleid" element={<PrivacyPolicy />} />
-			<Route path="/cookiebeleid" element={<CookiePolicy />} />
-			<Route path="/FAQ" element={<FAQ />} />
-			{/* Vacatures nest example */}
-			{/* <Route path="vacatures">
+		<Suspense fallback={<div>Loading...</div>}>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/menu" element={<Menu />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/nieuws" element={<News />} />
+				<Route path="/vacatures" element={<Vacatures />} />
+				<Route path="/privacybeleid" element={<PrivacyPolicy />} />
+				<Route path="/cookiebeleid" element={<CookiePolicy />} />
+				<Route path="/FAQ" element={<FAQ />} />
+				{/* Vacatures nest example */}
+				{/* <Route path="vacatures">
 						<Route index element={<Vacatures />} />
 						<Route path=":vacatureId" element={<Vacatures />} />
 						<Route path="*" element={<NotFound />} />
 					</Route> */}
-			<Route path="*" element={<NotFound />} />
-		</Routes>
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</Suspense>
 	);
 }
