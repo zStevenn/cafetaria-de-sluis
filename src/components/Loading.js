1018a6bcde08a593491eded5b22f3313db702d23
zStevenn <steventje678@hotmail.com>
2023-01-12 23:25:31 +0100

    Creates frontend for loading icon

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,3 +1,10 @@
+import { FaSpinner } from "react-icons/fa";
+
 export default function Loading() {
-	return <div>Loading...</div>;
+	return (
+		<div className="h-[60vh] w-screen flex flex-col justify-center items-center">
+			<FaSpinner className="animate-spin text-5xl text-gray-500" />
+			<p className="text-gray-500">Loading...</p>
+		</div>
+	);
 }
