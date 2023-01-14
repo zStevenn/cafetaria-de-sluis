1870d9d0dd33af1b94a53850b93a40a714e8f3ad
zStevenn <steventje678@hotmail.com>
2023-01-14 21:19:27 +0100

    inverted faq colors

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -5,10 +5,10 @@ export function FAQ({ question, answer }) {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<div className="px-2 py-4 bg-white text-primary rounded-lg mb-4 divide-y divide-primary">
+		<div className="px-2 py-4 bg-primary text-white rounded-lg mb-4 divide-y divide-white">
 			<button onClick={() => setIsOpen(!isOpen)}>
 				<p className="font-semibold text-base text-start flex flex-row flex-nowrap">
-					{question} <MdKeyboardArrowDown className="text-3xl fill-primary" />
+					{question} <MdKeyboardArrowDown className="text-3xl fill-white" />
 				</p>
 			</button>
 			{isOpen && <p className>{answer}</p>}
