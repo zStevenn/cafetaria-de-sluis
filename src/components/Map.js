cd2981cb77313ddb6720e776598111184e43b111
zStevenn <steventje678@hotmail.com>
2023-01-15 21:56:57 +0100

    Removed lazy and suspense from contact page

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,11 +3,11 @@ function Map(props) {
 		<iframe
 			className={props.className}
 			src="https://maps.google.com/maps?q=de%20sluis%2025&t=&z=15&ie=UTF8&iwloc=&output=embed"
-			frameborder="0"
+			frameBorder="0"
 			style={{ border: 0 }}
-			allowfullscreen="false"
+			allowFullscreen="false"
 			aria-hidden="false"
-			tabindex="0"
+			tabIndex="0"
 			title="Google Maps"
 		></iframe>
 	);
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,6 @@
-import { Title } from "../components/Text";
-import { ContactUs, ContactForm } from "../components/Contact";
-import { lazy, Suspense } from "react";
-import { AiOutlineLoading } from "react-icons/ai";
-
-// Lazy load the map
-const Map = lazy(() => import("../components/Map"));
+import { Title } from "../components/Text"
+import { ContactUs, ContactForm } from "../components/Contact"
+import Map from "../components/Map"
 
 export default function Contact() {
 	return (
@@ -12,11 +8,9 @@ export default function Contact() {
 			<Title text={"Contacteer ons!"} className="py-4" />
 			<div className="flex flex-col md:flex-row justify-center gap-4 m-8">
 				<ContactForm className="p-8 w-full md:w-1/2" />
-				<Suspense fallback={<AiOutlineLoading className="items-center text-6xl animate-spin text-primary"/>}>
-					<Map className="w-full h-[34rem] p-8 md:w-1/2 mb-8 md:mb-0 md:mr-8 bg-white rounded-xl" />
-				</Suspense>
+				<Map className="w-full h-[34rem] p-8 md:w-1/2 mb-8 md:mb-0 md:mr-8 bg-white rounded-xl" />
 			</div>
 			<ContactUs />
 		</div>
-	);
+	)
 }
