eb6e512d1d8bf71a951338d42262fe42caca9e20
zStevenn <steventje678@hotmail.com>
2023-01-15 21:32:58 +0100

    Creates under construction page

---
+++ src/pages/UnderConstruction.js
@@ -0,0 +1,11 @@
+const UnderConstruction = () => {
+    return (
+        <div className="bg-primary h-screen w-screen flex items-center justify-center">
+            <div className="text-center text-white text-3xl font-semibold">
+                Under Construction
+            </div>
+        </div>
+    )
+}
+
+export default UnderConstruction;
