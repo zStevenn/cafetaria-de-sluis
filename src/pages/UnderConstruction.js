53bdb8dcb5f3af78577df13ec58cabed03d5eb86
zStevenn <steventje678@hotmail.com>
2023-01-15 21:43:47 +0100

    Added text and button to under construction page

--- src/pages/UnderConstruction.js
+++ src/pages/UnderConstruction.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const UnderConstruction = () => {
     return (
-        <div className="bg-primary h-screen w-screen flex items-center justify-center">
-            <div className="text-center text-white text-3xl font-semibold">
-                Under Construction
+        <div className="bg-primary w-screen h-[70vh] flex items-center justify-center">
+            <div className="text-center text-white text-xl font-semibold">
+                <h1 className="mb-4">Deze pagina is nog in ontwikkeling.</h1>
+                <h2 className="mb-16">Sorry voor het ongemak</h2>
+                <Link to="/" className="p-4 bg-secondary rounded-lg hover:scale-110 transition-all">Terug naar de homepage</Link>
             </div>
         </div>
     )
