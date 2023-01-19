111b73bcadf431d4f741c41188883a082f8b2dab
Steven <steventje678@hotmail.com>
2023-01-19 14:21:59 +0100

    Update ContentWrapper.js

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -1,6 +1,6 @@
 function ContentWrapper({ children }) {
   return (
-    <div className="lg:w-3/5">
+    <div className="lg:w-3/5 lg:flex lg:flex-col lg:justify-center">
       {children}
     </div>
   )
