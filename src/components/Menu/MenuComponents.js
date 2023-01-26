6d8d64190ed575d01d338a2c32606af8fca1f775
Steven <steventje678@hotmail.com>
2023-01-26 11:28:16 +0100

    Changes to menu item
    Removes gap
    Places price in the center of the grid span

--- src/components/Menu/MenuComponents.js
+++ src/components/Menu/MenuComponents.js
@@ -6,13 +6,13 @@ export function MenuItem(props) {
   const formattedPrice = Number(props.price).toFixed(2).replace(".", ",");
 
   return (
-    <div className="grid grid-rows-2 grid-cols-3 grid-flow-col gap-4 py-3 px-2 bg-primary my-6 rounded-lg shadow shadow-primary">
+    <div className="grid grid-rows-2 grid-cols-3 grid-flow-col py-3 px-2 bg-primary my-6 rounded-lg shadow shadow-primary">
       {/* Item name */}
       <p className="col-span-2 capitalize text-white">{props.name}</p>
 	{/* Description */}
 	<p className="col-span-2 text-slate-300"></p>
       {/* Price */}
-      <p className="row-span-2 tracking-wide text-white">&euro; {formattedPrice}</p>
+      <p className="place-self-center row-span-2 tracking-wide text-white">&euro; {formattedPrice}</p>
     </div>
   );
 }
